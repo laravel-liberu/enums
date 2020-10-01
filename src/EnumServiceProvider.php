064c0f80d115594c80f437bbb7aafced1d4beedc
Mihai <mihai.ocneanu@gmail.com>
2020-10-01 09:59:54 +0300

    fixes translation issue with applications where using different language as the default (english)

--- src/EnumServiceProvider.php
+++ src/EnumServiceProvider.php
@@ -4,6 +4,7 @@ namespace LaravelEnso\Enums;
 
 use Illuminate\Support\ServiceProvider;
 use LaravelEnso\Enums\Facades\Enums;
+use LaravelEnso\Enums\Services\Enum;
 
 class EnumServiceProvider extends ServiceProvider
 {
@@ -11,6 +12,10 @@ class EnumServiceProvider extends ServiceProvider
 
     public function boot()
     {
+        Enum::localisation(false);
+
         Enums::register($this->register);
+
+        Enum::localisation(true);
     }
 }
